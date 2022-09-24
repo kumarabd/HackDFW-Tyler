17355316c882ca89c934404c5a3529aa409b9249
kumarabd <abishekkumar92@gmail.com>
2022-09-24 17:40:49 -0500

    added api
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import {useState} from 'react';
 import { TextField, Box } from '@mui/material';
 
 function App() {
+  const inputProps = {
+    step: 300,
+  }
+  
+  const [data, setData] = useState({});
+
+  const handleTextInputChange = async e => {
+    const response = await fetch(`http://127.0.0.1:8081/api/input`, {
+            method: 'POST',
+            crossDomain:true,
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({data: e.target.value})
+          })
+          setData(await response.json().data)
+  }
+
   return (
     <div className="App">
       <p>
@@ -16,8 +33,8 @@ function App() {
       noValidate
       autoComplete="off"
     >
-        <TextField id="outlined-basic" label="Input" variant="outlined" color="primary" />
-        <TextField id="outlined-basic" label="Output" variant="outlined" color="primary" />
+        <TextField id="input" label="Input" variant="outlined" color="primary" onChange={handleTextInputChange}/>
+        <TextField id="output" label="Output" variant="outlined" color="primary" inputProps={{...inputProps, value: data, readOnly:true}}/>
         </Box>
       </header>
     </div>
